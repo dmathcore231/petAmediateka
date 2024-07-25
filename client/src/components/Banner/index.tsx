901831cb7d7523c35bf11034b421ba44b8e5f38f
John Doe <dmathcore231@gmail.com>
2024-07-25 14:08:37 +0300

    refactorinf(client): banner styles

--- client/src/components/Banner/index.tsx
+++ client/src/components/Banner/index.tsx
@@ -4,30 +4,32 @@ import { BannerProps } from "../../types/interfaces/BannerProps"
 export function Banner({ title, bannerListItem, img, titleBtn, ageRestriction }: BannerProps): JSX.Element {
   return (
     <div className="banner">
-      <div className="banner__item">
-        <div className="banner__title">
-          <h2>{title}</h2>
-        </div>
-        <ul className="banner-list">
-          {bannerListItem.map((item, index) => (
-            <li key={index} className="banner-list__item title title_size_sm">
-              {item}
+      <div className="banner__wrapper">
+        <div className="banner__item">
+          <div className="banner__title">
+            <h2>{title}</h2>
+          </div>
+          <ul className="banner-list">
+            {bannerListItem.map((item, index) => (
+              <li key={index} className="banner-list__item title title_size_sm">
+                {item}
+              </li>
+            ))}
+            <li className="banner-list__item title title_size_sm">
+              {`${ageRestriction}+`}
             </li>
-          ))}
-          <li className="banner-list__item title title_size_sm">
-            {`${ageRestriction}+`}
-          </li>
-        </ul>
-        <div className="banner__btn">
-          <Link to={'#'} className="btn btn_primary"
-            onClick={() => console.log('click')}
-          >
-            {titleBtn}
-          </Link>
+          </ul>
+          <div className="banner__btn">
+            <Link to={'#'} className="btn btn_primary"
+              onClick={() => console.log('click')}
+            >
+              {titleBtn}
+            </Link>
+          </div>
         </div>
-      </div>
-      <div className="banner__item banner__item_position_absolute">
-        <img src={img} alt="" />
+        <picture className="banner-picture">
+          <img src={img} alt="" className="banner-picture__img" />
+        </picture>
       </div>
     </div>
   )
